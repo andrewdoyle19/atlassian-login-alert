3fd5e6efac1064d51a38949f87d66c635ce760ca
Andrew Doyle <andrew.doyle.83@gmail.com>
2017-02-21 13:03:40 +1100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Atlassian Login Alert
+
+How annoying is it when you post stuff as Anonymous in JIRA, simply because you weren't logged in.
+
+Install this extension and get an awesome notification! (complete with flashing lights)
+
+![alt]('/img/alert-example.png')
+
+## Installation
+
+1. Update the `src/manifest.json` file to add the path to your JIRA server
+
+2. Load as an unpacked extension in Chrome
+`chrome://extensions`
+
---
+++ src/myscript.js
@@ -0,0 +1,26 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+var login = document.getElementsByClassName("login-link");
+if(!login || login.length === 0) {
+	login = document.getElementById("login-link");
+}
+if(login){
+	
+	//var dashboard = document.getElementsByClassName('aui-page-header');
+	var header = document.getElementsByTagName('header');
+	if(header){
+		var logo = chrome.extension.getURL('/img/flashing-light.gif');
+		var div = document.createElement('div');
+		div.innerHTML = '<img src="' + logo + '" /> Alert: You are not logged in! <img src="' + logo + '" />';	
+		div.className = 'login-alert';		
+		insertAfter(header[0], div)
+	}
+}
+
+
+    
+	
+   
+
